ff9bf6154a20bb174ea8a5b29c3393b22ea6473c
Tobias Schweizer <t.schweizer@unibas.ch>
2019-07-26 12:24:56 +0200

    test (auth endpoint):  add test for logout

--- src/api/v2/authentication/authentication-endpoint.spec.ts
+++ src/api/v2/authentication/authentication-endpoint.spec.ts
@@ -1,7 +1,7 @@
 import {KnoraApiConfig} from '../../../knora-api-config';
 import {KnoraApiConnection} from '../../../knora-api-connection';
 import {MockAjaxCall} from '../../../../test/mockajaxcall';
-import {ApiResponseData, LoginResponse} from '../../..';
+import {ApiResponseData, LoginResponse, LogoutResponse} from '../../..';
 
 describe('Test class AuthenticationEndpoint', () => {
 
@@ -13,7 +13,7 @@ describe('Test class AuthenticationEndpoint', () => {
         jasmine.Ajax.uninstall();
     });
 
-    it('should perfrom a login', done => {
+    it('should perform a login', done => {
 
         const config = new KnoraApiConfig('http', 'localhost', 3333);
 
@@ -36,6 +36,31 @@ describe('Test class AuthenticationEndpoint', () => {
         expect(request.method).toEqual('POST');
 
         expect(request.data()).toEqual({username: 'user', password: 'test'});
+
+    });
+
+    it('should perform a logout', done => {
+
+        const config = new KnoraApiConfig('http', 'localhost', 3333);
+
+        const knoraApiConnection = new KnoraApiConnection(config);
+
+        knoraApiConnection.v2.auth.logout().subscribe((response: ApiResponseData<LogoutResponse>) => {
+
+            expect(response.body.status).toEqual(0);
+            expect(response.body.message).toEqual('Logout OK');
+            expect(knoraApiConnection.v2.jsonWebToken).toEqual('');
+
+            done();
+        });
+
+        const request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith(MockAjaxCall.mockResponse(JSON.stringify({"message":"Logout OK","status":0})));
+
+        expect(request.url).toEqual('http://localhost:3333/v2/authentication');
+
+        expect(request.method).toEqual('DELETE');
 
     });
 
