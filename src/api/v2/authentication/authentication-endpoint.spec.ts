68af95164f9fc92a943b8ff8150ebf28eae0592d
Tobias Schweizer <t.schweizer@unibas.ch>
2019-07-26 12:18:58 +0200

    test (auth endpoint):  add test for login

---
+++ src/api/v2/authentication/authentication-endpoint.spec.ts
@@ -0,0 +1,43 @@
+import {KnoraApiConfig} from '../../../knora-api-config';
+import {KnoraApiConnection} from '../../../knora-api-connection';
+import {MockAjaxCall} from '../../../../test/mockajaxcall';
+import {ApiResponseData, LoginResponse} from '../../..';
+
+describe('Test class AuthenticationEndpoint', () => {
+
+    beforeEach(() => {
+        jasmine.Ajax.install();
+    });
+
+    afterEach(() => {
+        jasmine.Ajax.uninstall();
+    });
+
+    it('should perfrom a login', done => {
+
+        const config = new KnoraApiConfig('http', 'localhost', 3333);
+
+        const knoraApiConnection = new KnoraApiConnection(config);
+
+        knoraApiConnection.v2.auth.login('user', 'test').subscribe((response: ApiResponseData<LoginResponse>) => {
+
+            expect(response.body.token).toEqual('testtoken');
+            expect(knoraApiConnection.v2.jsonWebToken).toEqual('testtoken');
+
+            done();
+        });
+
+        const request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith(MockAjaxCall.mockResponse(JSON.stringify({'token': 'testtoken'})));
+
+        expect(request.url).toEqual('http://localhost:3333/v2/authentication');
+
+        expect(request.method).toEqual('POST');
+
+        expect(request.data()).toEqual({username: 'user', password: 'test'});
+
+    });
+
+});
+
