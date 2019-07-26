55d7a24fb86efc2022345c1eaf20f6e35a73fad4
Tobias Schweizer <t.schweizer@unibas.ch>
2019-07-26 10:28:02 +0200

    test (endpoint):  add test for httGet with token

--- src/api/endpoint.spec.ts
+++ src/api/endpoint.spec.ts
@@ -1,17 +1,11 @@
 import {KnoraApiConfig} from '../knora-api-config';
 import {Endpoint} from './endpoint';
-import {ApiResponseData} from '..';
 import {MockAjaxCall} from '../../test/mockajaxcall';
 import {AjaxResponse} from 'rxjs/ajax';
 
-
 describe('Endpoint', () => {
 
-    const config = new KnoraApiConfig('http', 'localhost', 3333);
-    const endpoint = new Endpoint(config, '/test');
-
     beforeEach(() => {
-
         jasmine.Ajax.install();
     });
 
@@ -19,29 +13,58 @@ describe('Endpoint', () => {
         jasmine.Ajax.uninstall();
     });
 
-    describe('Method httpGet', () => {
 
-        it('should perform a get request', done => {
+    it('should perform a get request', done => {
 
-            endpoint['httpGet']().subscribe(
-                    (response: AjaxResponse) => {
-                        expect(response.status).toEqual(200);
-                        expect(response.response).toEqual({test: 'test'});
+        const config = new KnoraApiConfig('http', 'localhost', 3333);
 
-                        done();
-                    });
+        const endpoint = new Endpoint(config, '/test');
 
-            const request = jasmine.Ajax.requests.mostRecent();
+        endpoint['httpGet']().subscribe(
+                (response: AjaxResponse) => {
+                    expect(response.status).toEqual(200);
+                    expect(response.response).toEqual({test: 'test'});
 
-            request.respondWith(MockAjaxCall.mockResponse(JSON.stringify({test: 'test'})));
+                    done();
+                });
 
-            expect(request.url).toBe('http://localhost:3333/test');
+        const request = jasmine.Ajax.requests.mostRecent();
 
-            expect(request.method).toEqual('GET');
+        request.respondWith(MockAjaxCall.mockResponse(JSON.stringify({test: 'test'})));
 
-            expect(request.requestHeaders).toEqual({});
+        expect(request.url).toBe('http://localhost:3333/test');
 
-        });
+        expect(request.method).toEqual('GET');
+
+        expect(request.requestHeaders).toEqual({});
+
+    });
+
+    it('should perform a get request with authentication', done => {
+
+        const config = new KnoraApiConfig('http', 'localhost', 3333);
+
+        const endpoint = new Endpoint(config, '/test');
+
+        endpoint.jsonWebToken = 'testtoken';
+
+        endpoint['httpGet']().subscribe(
+                (response: AjaxResponse) => {
+                    expect(response.status).toEqual(200);
+                    expect(response.response).toEqual({test: 'test'});
+
+                    done();
+                });
+
+        const request = jasmine.Ajax.requests.mostRecent();
+
+        request.respondWith(MockAjaxCall.mockResponse(JSON.stringify({test: 'test'})));
+
+        expect(request.url).toBe('http://localhost:3333/test');
+
+        expect(request.method).toEqual('GET');
+
+        expect(request.requestHeaders).toEqual({Authorization: 'Bearer testtoken'});
 
     });
 
