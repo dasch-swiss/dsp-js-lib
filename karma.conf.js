2194b4e4c27503eb3e6fcf86ad710fa6d6d07c11
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2019-08-07 16:55:12 +0200

    Refacor (karma) config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
     config.set({
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_ERROR,
         plugins: [
             "karma-jasmine",
             "karma-mocha-reporter",
