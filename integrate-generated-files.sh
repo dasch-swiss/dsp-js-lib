eb238a39cbff8a91db56448b24f00c7759682050
Tobias Schweizer <t.schweizer@unibas.ch>
2019-08-29 09:35:46 +0200

    feature (admin): add script to copy generated files

---
+++ integrate-generated-files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+usage() {
+  echo "Usage: $0 <generate-files-dir>" 1>&2;
+  echo "Run this script from project root: ./integrate-generated-files.sh"
+  exit 1;
+  }
+
+if [ $# -eq 1 ] && [ -d $1 ]
+then
+  cp -r $1/api/admin/ ./src/api/admin/
+  cp -r $1/models/admin/ ./src/models/admin/
+  cp -r $1/test-data/admin/ ./test/data/api/admin/
+else
+  usage
+fi
+
+
