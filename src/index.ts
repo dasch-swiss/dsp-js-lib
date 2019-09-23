24b75888d68847d0bea6d8a719aad0db25409dc2
Tobias Schweizer <t.schweizer@unibas.ch>
2019-09-23 09:43:45 +0200

    refactor (resource): export classes

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export { Permissions } from "./models/admin/permissions";
 export { Project } from "./models/admin/project";
 export { User } from "./models/admin/user";
 export { UserResponse } from "./models/admin/user-response";
+export { UsersResponse } from "./models/admin/users-response";
 
 export { LoginResponse } from "./models/v2/authentication/login-response";
 export { LogoutResponse } from "./models/v2/authentication/logout-response";
@@ -19,3 +20,19 @@ export { ListNodeCache } from "./cache/ListNodeCache";
 export { ApiResponse } from "./models/api-response";
 export { ApiResponseData } from "./models/api-response-data";
 export { ApiResponseError } from "./models/api-response-error";
+
+export { ReadResource } from "./models/v2/resources/read-resource";
+
+export { ReadBooleanValue } from "./models/v2/resources/values/read-boolean-value";
+export { ReadColorValue } from "./models/v2/resources/values/read-color-value";
+export { ReadDateValue } from "./models/v2/resources/values/read-date-value";
+export { ReadDecimalValue } from "./models/v2/resources/values/read-decimal-value";
+export { ReadFileValue, ReadStillImageFileValue } from "./models/v2/resources/values/read-file-value";
+export { ReadGeomValue } from "./models/v2/resources/values/read-geom-value";
+export { ReadIntValue } from "./models/v2/resources/values/read-int-value";
+export { ReadIntervalValue } from "./models/v2/resources/values/read-interval-value";
+export { ReadLinkValue } from "./models/v2/resources/values/read-link-value";
+export { ReadListValue } from "./models/v2/resources/values/read-list-value";
+export { ReadTextValue, ReadTextValueAsString, ReadTextValueAsHtml, ReadTextValueAsXml } from "./models/v2/resources/values/read-text-value";
+export { ReadUriValue } from "./models/v2/resources/values/read-uri-value";
+export { ReadValue } from "./models/v2/resources/values/read-value";
