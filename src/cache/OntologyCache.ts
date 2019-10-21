f0ef1d0c7265be4eef800b788614fdfc8ff61530
Tobias Schweizer <t.schweizer@unibas.ch>
2019-10-21 14:03:07 +0200

    docs (ontology): add doc strings

--- src/cache/OntologyCache.ts
+++ src/cache/OntologyCache.ts
@@ -8,11 +8,19 @@ import { ReadOntology } from "../models/v2/ontologies/read-ontology";
 import { GenericCache } from "./GenericCache";
 
 /**
- * Contains resource class definitions
+ * Represents resource class definitions
  * and property definitions the resource classes have cardinalities for.
  */
 export interface IResourceClassAndPropertyDefinitions {
+
+    /**
+     * Resource class definitions and their cardinalities.
+     */
     classes: { [index: string]: ClassDefinition };
+
+    /**
+     * Property definitions referred to in cardinalities.
+     */
     properties: { [index: string]: PropertyDefinition };
 }
 
