62e98facaa2bfa8048c92844a76243d5c84cea42
Tobias Schweizer <t.schweizer@unibas.ch>
2019-10-21 09:53:22 +0200

    test (projects): add test for create project

--- src/api/admin/projects/projects-endpoint.spec.ts
+++ src/api/admin/projects/projects-endpoint.spec.ts
@@ -1,8 +1,10 @@
-import { ApiResponseData } from "../../..";
+import { ApiResponseData, ApiResponseError, Project, UserResponse } from "../../..";
 import { MockAjaxCall } from "../../../../test/mockajaxcall";
 import { KnoraApiConfig } from "../../../knora-api-config";
 import { KnoraApiConnection } from "../../../knora-api-connection";
+import { ProjectResponse } from "../../../models/admin/project-response";
 import { ProjectsResponse } from "../../../models/admin/projects-response";
+import { StringLiteral } from "../../../models/admin/string-literal";
 
 describe("ProjectsEndpoint", () => {
 
@@ -39,6 +41,53 @@ describe("ProjectsEndpoint", () => {
             expect(request.url).toBe("http://localhost:3333/admin/projects");
 
             expect(request.method).toEqual("GET");
+
+        });
+
+    });
+
+    describe("Method createProject", () => {
+
+        it("should create a project", done => {
+
+            const project = new Project();
+            project.shortname = "newproject";
+            project.shortcode = "1111";
+            project.longname = "project longname";
+
+            const description = new StringLiteral();
+            description.language = "en";
+            description.value = "project description";
+
+            project.description = [description];
+
+            project.keywords = ["keywords"];
+            project.logo = "/fu/bar/baz.jpg";
+            project.status = true;
+            project.selfjoin = false;
+
+            knoraApiConnection.admin.projectsEndpoint.createProject(project).subscribe(
+                (response: ApiResponseData<ProjectResponse> | ApiResponseError) => {
+
+                    done();
+                }
+            );
+
+            const request = jasmine.Ajax.requests.mostRecent();
+
+            const user = require("../../../../test/data/api/admin/projects/get-project-response.json");
+
+            request.respondWith(MockAjaxCall.mockResponse(JSON.stringify(user)));
+
+            expect(request.url).toBe("http://localhost:3333/admin/projects");
+
+            expect(request.method).toEqual("POST");
+
+            expect(request.requestHeaders).toEqual({"Content-Type": "application/json; charset=utf-8"});
+
+            const payload = require("../../../../test/data/api/admin/projects/create-project-request.json");
+
+            expect(request.data()).toEqual(payload);
 
         });
 
