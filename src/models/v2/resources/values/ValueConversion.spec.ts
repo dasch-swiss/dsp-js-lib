883fdc9ed6b2140f0708a8e4f134822fafc17c07
Tobias Schweizer <t.schweizer@unibas.ch>
2019-10-31 09:55:57 +0100

    feature (create values v2): start on creating int values

--- src/models/v2/resources/values/ValueConversion.spec.ts
+++ src/models/v2/resources/values/ValueConversion.spec.ts
@@ -1,6 +1,7 @@
 import { JsonConvert, OperationMode, ValueCheckingMode } from "json2typescript";
 import { PropertyMatchingRule } from "json2typescript/src/json2typescript/json-convert-enums";
 import { Constants } from "../../Constants";
+import { CreateIntValue } from "./create/create-int-value";
 import { UpdateIntValue } from "./update/update-int-value";
 
 describe("ValueConversion", () => {
@@ -39,5 +40,25 @@ describe("ValueConversion", () => {
         expect(result).toEqual(expectedResult);
 
     });
+
+    it("create an Integer Value without comment", () => {
+
+        const createIntVal = new CreateIntValue();
+
+        createIntVal.type = Constants.IntValue;
+        createIntVal.int = 1;
+
+        const result: any = jsonConvert.serializeObject(createIntVal);
+
+        const expectedResult = {
+            "@type": "http://api.knora.org/ontology/knora-api/v2#IntValue",
+            "http://api.knora.org/ontology/knora-api/v2#intValueAsInt": 1
+        };
+
+        expect(result).toEqual(expectedResult);
+
+    });
+
+
 
 });
---
+++ src/models/v2/resources/values/create/create-value.ts
@@ -0,0 +1,4 @@
+import { BaseValue } from "../base-value";
+
+export abstract class CreateValue extends BaseValue {
+}
