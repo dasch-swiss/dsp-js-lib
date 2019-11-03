fa2112781ac7787badb0fcddb5bf821efd4f14d0
Tobias Schweizer <t.schweizer@unibas.ch>
2019-11-03 18:42:24 +0100

    refactor (update values): add space

--- src/models/v2/resources/values/update/update-int-value.ts
+++ src/models/v2/resources/values/update/update-int-value.ts
@@ -4,7 +4,7 @@ import { IBaseIntValue } from "../type-specific-interfaces/base-int-value";
 import { UpdateValue } from "./update-value";
 
 @JsonObject("UpdateIntValue")
-export class UpdateIntValue extends UpdateValue implements IBaseIntValue{
+export class UpdateIntValue extends UpdateValue implements IBaseIntValue {
 
     @JsonProperty(Constants.IntValueAsInt, Number)
     int: number = 0;
