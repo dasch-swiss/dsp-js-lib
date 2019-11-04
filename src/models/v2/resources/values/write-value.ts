e07ada49e6131030dd0be07d1db4f837c3d70740
Tobias Schweizer <t.schweizer@unibas.ch>
2019-11-04 15:18:03 +0100

    fix (values): fix decorator name

--- src/models/v2/resources/values/write-value.ts
+++ src/models/v2/resources/values/write-value.ts
@@ -2,7 +2,7 @@ import { JsonObject, JsonProperty } from "json2typescript";
 import { Constants } from "../../Constants";
 import { BaseValue } from "./base-value";
 
-@JsonObject("ReadWriteValue")
+@JsonObject("WriteValue")
 export abstract class WriteValue extends BaseValue {
 
     @JsonProperty(Constants.HasPermissions, String, true)
