93b5fc65263509479052c30d52d7e6322825f383
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-11-07 14:11:01 +0100

    feat(admin): Add custom converters for collection types (#99)

---
+++ src/models/admin/custom-converters/administrative-permissions-per-project-converter.ts
@@ -0,0 +1,37 @@
+import { JsonConvert, JsonConverter, JsonCustomConvert, OperationMode, ValueCheckingMode } from "json2typescript";
+import { PropertyMatchingRule } from "json2typescript/src/json2typescript/json-convert-enums";
+
+import { Permission } from "../permission";
+
+@JsonConverter
+export class AdministrativePermissionsPerProjectConverter implements JsonCustomConvert<{ [key: string]: Permission[] }> {
+
+    static jsonConvert: JsonConvert = new JsonConvert(
+        OperationMode.ENABLE,
+        ValueCheckingMode.DISALLOW_NULL,
+        false,
+        PropertyMatchingRule.CASE_STRICT
+    );
+
+    serialize(permissions: { [key: string]: Permission[] }): any {
+        return {};
+    }
+
+    deserialize(item: any): { [key: string]: Permission[] } {
+        const outputObj: { [key: string]: Permission[] } = {};
+        const keys = Object.keys(item);
+
+        keys.forEach((key: string) => {
+            const permInputArray = item[key];
+
+            outputObj[key] = permInputArray.map(
+                (element: any) => {
+                    return AdministrativePermissionsPerProjectConverter.jsonConvert.deserializeObject(element, Permission);
+
+                }
+            );
+        });
+
+        return outputObj;
+    }
+}
