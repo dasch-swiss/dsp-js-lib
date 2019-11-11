87856b344e3335951e7e39c9042761b2371375f8
Tobias Schweizer <t.schweizer@unibas.ch>
2019-11-11 10:39:42 +0100

    fix (index.ts): export missing members

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,12 @@ export { ApiResponseError } from "./models/api-response-error";
 
 export { ReadOntology } from "./models/v2/ontologies/read-ontology";
 export { OntologyMetadata, OntologiesMetadata } from "./models/v2/ontologies/ontology-metadata";
+export { IResourceClassAndPropertyDefinitions } from "./cache/OntologyCache";
+export { ClassDefinition, IHasProperty, Cardinality } from "./models/v2/ontologies/class-definition";
+export { SystemPropertyDefinition } from "./models/v2/ontologies/system-property-definition";
+export { ResourcePropertyDefinition } from "./models/v2/ontologies/resource-property-definition";
+export { ResourceClassDefinition } from "./models/v2/ontologies/resource-class-definition";
+export { StandoffClassDefinition } from "./models/v2/ontologies/standoff-class-definition";
 export { ReadResource } from "./models/v2/resources/read-resource";
 export { CountQueryResponse} from "./models/v2/search/count-query-response";
 
