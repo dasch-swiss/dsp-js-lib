8aa1b657da66f454905d26e3b37b667e0bf0abfd
Tobias Schweizer <t.schweizer@unibas.ch>
2019-12-16 12:34:05 +0100

    test (admin): add tests for lists

--- src/api/admin/lists/lists-endpoint.spec.ts
+++ src/api/admin/lists/lists-endpoint.spec.ts
@@ -3,8 +3,10 @@ import { MockAjaxCall } from "../../../../test/mockajaxcall";
 import { KnoraApiConfig } from "../../../knora-api-config";
 import { KnoraApiConnection } from "../../../knora-api-connection";
 import { CreateListRequest } from "../../../models/admin/create-list-request";
+import { ListInfoResponse } from "../../../models/admin/list-info-response";
 import { ListResponse } from "../../../models/admin/list-response";
 import { ListsResponse } from "../../../models/admin/lists-response";
+import { UpdateListInfoRequest } from "../../../models/admin/update-list-info-request";
 
 describe("ListsEndpoint", () => {
 
@@ -130,6 +132,61 @@ describe("ListsEndpoint", () => {
             expect(request.url).toBe("http://localhost:3333/admin/lists/http%3A%2F%2Frdfh.ch%2Flists%2F0001%2FtreeList");
 
             expect(request.method).toEqual("GET");
+
+        });
+
+    });
+
+    describe("Method UpdateListInfo", () => {
+
+        it("should update a list", done => {
+
+            const listInfo = new UpdateListInfoRequest();
+
+            listInfo.listIri = "http://rdfh.ch/lists/0001/treeList01";
+            listInfo.projectIri = "http://rdfh.ch/projects/00FF";
+
+            const label1 = new StringLiteral();
+            label1.language = "de";
+            label1.value = "Neue geänderte Liste";
+
+            const label2 = new StringLiteral();
+            label2.language = "en";
+            label2.value = "Changed list";
+
+            listInfo.labels = [label1, label2];
+
+            const comment1 = new StringLiteral();
+            comment1.language = "de";
+            comment1.value = "Neuer Kommentar";
+
+            const comment2 = new StringLiteral();
+            comment2.language = "en";
+            comment2.value = "New comment";
+
+            listInfo.comments = [comment1, comment2];
+
+            knoraApiConnection.admin.listsEndpoint.updateListInfo(listInfo).subscribe(
+                (res: ApiResponseData<ListInfoResponse>) => {
+                    done();
+                }
+            );
+
+            const request = jasmine.Ajax.requests.mostRecent();
+
+            const listsResponse = require("../../../../test/data/api/admin/lists/get-list-info-response.json");
+
+            request.respondWith(MockAjaxCall.mockResponse(JSON.stringify(listsResponse)));
+
+            expect(request.url).toBe("http://localhost:3333/admin/lists/http%3A%2F%2Frdfh.ch%2Flists%2F0001%2FtreeList01");
+
+            expect(request.method).toEqual("PUT");
+
+            expect(request.requestHeaders).toEqual({"Content-Type": "application/json; charset=utf-8"});
+
+            const payload = require("../../../../test/data/api/admin/lists/update-list-info-request.json");
+
+            expect(request.data()).toEqual(payload);
 
         });
 
