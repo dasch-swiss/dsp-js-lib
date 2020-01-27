1111b70bb08015866029ca95019fb14c7fb63ff6
André Kilchenmann <github@milchkannen.ch>
2020-01-27 17:47:37 +0100

    feat(ontology): Add label to ReadOntology

--- src/models/api-response-error.ts
+++ src/models/api-response-error.ts
@@ -51,6 +51,8 @@ export class ApiResponseError extends ApiResponse {
      */
     static fromAjaxError(ajaxError: AjaxError): ApiResponseError {
 
+        // console.warn(ajaxError)
+
         const response = new ApiResponseError();
 
         if (ajaxError.request.method) response.method = ajaxError.request.method;
