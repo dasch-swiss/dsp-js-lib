830f34e266b305c6be1fbaab7a1fa9a3c8d6b3ae
Tobias Schweizer <t.schweizer@unibas.ch>
2020-05-06 17:27:55 +0200

    chore (dev pack): prepare dev package

--- scripts/prepare-dev-publication.js
+++ scripts/prepare-dev-publication.js
@@ -3,7 +3,7 @@
 const fs = require('fs-extra');
 
 // adapt package.json
-const pack = fs.readJsonSync('./package.json')
+const pack = fs.readJsonSync('./package.json');
 pack.files = pack.files.concat(
     [
         "test/**/*.d.ts",
@@ -11,9 +11,13 @@ pack.files = pack.files.concat(
         "test/**/*.js",
         "test/**/*.json"]
 );
-console.log(pack.files)
 fs.writeJsonSync('./package.json', pack);
 
 // adapt tsconfig.json
+const tsconf = fs.readJsonSync('./tsconfig.json');
+// get index of "test" in excluded files array
+const indexOfTest = tsconf.exclude.indexOf('test')
+tsconf.exclude.splice(indexOfTest, 1);
+fs.writeJsonSync('./tsconfig.json', tsconf);
 
 // add exports to index.ts
