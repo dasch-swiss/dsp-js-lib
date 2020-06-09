e18395ea5d2d6cb5ce1588317cdd94066692ad81
Loïc Jaouen <loic.jaouen@unil.ch>
2020-06-09 15:49:18 +0200

    added remote host tests

--- src/models/v2/ontologies/OntologyConversionUtil.spec.ts
+++ src/models/v2/ontologies/OntologyConversionUtil.spec.ts
@@ -12,28 +12,63 @@ describe("OntologyConversionUtil", () => {
     });
 
     describe("Method getOntologyIriFromEntityIri", () => {
-
-        const config = new KnoraApiConfig("http", "0.0.0.0", 3333);
+        const localConfig = new KnoraApiConfig("http", "0.0.0.0", 3333);
+        const remoteConfig = new KnoraApiConfig("https", "test.example.com", 3333);
 
         it("should get the ontology IRI from a knora-api entity", () => {
-            const ontologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri("http://api.knora.org/ontology/knora-api/v2#Resource", config);
+            const localOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://api.knora.org/ontology/knora-api/v2#Resource",
+                localConfig
+            );
 
-            expect(ontologyIri[0]).toEqual("http://api.knora.org/ontology/knora-api/v2");
+            expect(localOntologyIri[0]).toEqual(
+                "http://api.knora.org/ontology/knora-api/v2"
+            );
 
+            const remoteOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://api.knora.org/ontology/knora-api/v2#Resource",
+                remoteConfig
+            );
+
+            expect(remoteOntologyIri[0]).toEqual(
+              "http://api.knora.org/ontology/knora-api/v2"
+            );
         });
 
         it("should get the ontology IRI from a project entity", () => {
-            const ontologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri("http://0.0.0.0:3333/ontology/0001/anything/v2#Thing", config);
+            const localOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://0.0.0.0:3333/ontology/0001/anything/v2#Thing",
+                localConfig
+            );
 
-            expect(ontologyIri[0]).toEqual("http://0.0.0.0:3333/ontology/0001/anything/v2");
+            expect(localOntologyIri[0]).toEqual(
+                "http://0.0.0.0:3333/ontology/0001/anything/v2"
+            );
 
+            const remoteOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://test.example.com/ontology/0001/anything/v2#Thing",
+                remoteConfig
+            );
+
+            expect(remoteOntologyIri[0]).toEqual(
+                "http://test.example.com/ontology/0001/anything/v2"
+            );
         });
 
         it("should ignore an external entity", () => {
-            const ontologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri("http://www.w3.org/2000/01/rdf-schema#label", config);
+            const localOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://www.w3.org/2000/01/rdf-schema#label",
+                localConfig
+            );
 
-            expect(ontologyIri.length).toEqual(0);
+            expect(localOntologyIri.length).toEqual(0);
 
+            const remoteOntologyIri = OntologyConversionUtil.getOntologyIriFromEntityIri(
+                "http://www.w3.org/2000/01/rdf-schema#label",
+                remoteConfig
+            );
+
+            expect(remoteOntologyIri.length).toEqual(0);
         });
 
     });
