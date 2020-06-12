efa268924e486da8a9b4bf378e858ddbf228ddc8
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-12 10:14:02 +0200

    test (mock ontology): mock ontologies metadata

--- test-framework/src/app/app.component.ts
+++ test-framework/src/app/app.component.ts
@@ -26,7 +26,8 @@ import {
     UserResponse,
     UsersResponse,
     WriteValueResponse,
-    DeleteResourceResponse
+    DeleteResourceResponse,
+    MockOntology
 } from "@dasch-swiss/dsp-js";
 
 import { map } from "rxjs/operators";
@@ -62,6 +63,9 @@ export class AppComponent implements OnInit {
     this.knoraApiConnection = new KnoraApiConnection(config);
     // console.log(this.knoraApiConnection);
     this.userCache = new UserCache(this.knoraApiConnection);
+
+    console.log(MockOntology.mockReadOntology("http://0.0.0.0:3333/ontology/0001/anything/v2"));
+    console.log(MockOntology.mockOntologiesMetadata())
   }
 
   login() {
