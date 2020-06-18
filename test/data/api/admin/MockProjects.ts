a6633039d3778a2492555dc3a7547d7405062867
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-18 17:37:06 +0200

    feature (admin): add mock for projects

--- test/data/api/admin/MockProjects.ts
+++ test/data/api/admin/MockProjects.ts
@@ -1,5 +1,7 @@
 import { JsonConvert, OperationMode, ValueCheckingMode } from "json2typescript";
 import { PropertyMatchingRule } from "json2typescript/src/json2typescript/json-convert-enums";
+import { AjaxResponse } from "rxjs/ajax";
+import { ApiResponseData } from "../../../../src/models/api-response-data";
 import { ProjectsResponse } from "../../../../src/models/admin/projects-response";
 
 import projects from "./projects/get-projects-response.json";
@@ -13,7 +15,11 @@ export namespace MockProjects {
         PropertyMatchingRule.CASE_STRICT
     );
 
-    export const mockProjects = (): ProjectsResponse => {
-        return jsonConvert.deserializeObject(projects, ProjectsResponse);
+    export const mockProjects = (): ApiResponseData<ProjectsResponse> => {
+        const responseData = ApiResponseData.fromAjaxResponse(new AjaxResponse({} as any, {} as any, {}));
+
+        const projectsRes = jsonConvert.serializeObject(projects, ProjectsResponse);
+        responseData.body = projectsRes;
+        return responseData as ApiResponseData<ProjectsResponse>;
     };
 }
