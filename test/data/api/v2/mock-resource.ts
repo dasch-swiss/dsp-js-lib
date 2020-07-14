723dfbb523b689d288d4f2c8ac02220c4aae12d0
André Kilchenmann <github@milchkannen.ch>
2020-07-14 15:39:38 +0200

    test: Mock resources

--- test/data/api/v2/mock-resource.ts
+++ test/data/api/v2/mock-resource.ts
@@ -1,6 +1,6 @@
 import { JsonConvert, OperationMode, ValueCheckingMode } from "json2typescript";
 import { PropertyMatchingRule } from "json2typescript/src/json2typescript/json-convert-enums";
-import { Observable, of } from "rxjs";
+import { Observable, of, forkJoin } from "rxjs";
 import { map } from "rxjs/operators";
 import { ReadResource } from "../../../../src/models/v2/resources/read/read-resource";
 import { V2Endpoint } from "../../../../src/api/v2/v2-endpoint";
@@ -54,6 +54,24 @@ export namespace MockResource {
         return resSeq.pipe(
             map(
                 (seq: ReadResourceSequence) => seq.resources[0]
+            )
+        );
+
+    };
+
+    export const getTesthings = (length = 25): Observable<ReadResourceSequence> => {
+
+        const resources: Array<Observable<ReadResource>> = new Array(length).fill(0).map(
+            () => {
+                return getTestthing();
+            }
+        );
+
+        return forkJoin(resources).pipe(
+            map(
+                (res: ReadResource[]) => {
+                    return new ReadResourceSequence(res);
+                }
             )
         );
 
