dbdf6fde9b34f952c3d01de3546326c4fcdce839
André Kilchenmann <github@milchkannen.ch>
2020-07-14 22:27:55 +0200

    fix: Undo previous changes

--- src/models/admin/string-literal.ts
+++ src/models/admin/string-literal.ts
@@ -9,13 +9,13 @@ export class StringLiteral {
     /**
      * The language of a string literal.
      */
-    @JsonProperty("@language", String, true)
+    @JsonProperty("language", String, true)
     language?: string = undefined;
 
     /**
      * The value of a string literal.
      */
-    @JsonProperty("@value", String)
+    @JsonProperty("value", String)
     value: string = "";
 
 }
