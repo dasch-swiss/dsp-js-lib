0277ee5d8efc0e77b50c9ebc54e982722e850dbd
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-07-23 10:16:46 +0200

    build: bump dsp-api

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
 export * from "./src/index";
-export { MockOntology } from "./test/data/api/v2/mock-ontology";
-export { MockList } from "./test/data/api/v2/mock-list";
-export { MockResource } from "./test/data/api/v2/mock-resource";
-export { MockProjects } from "./test/data/api/admin/mock-projects";
-export { MockUsers } from "./test/data/api/admin/mock-users";
