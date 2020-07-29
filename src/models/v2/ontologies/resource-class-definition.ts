a9652781ae4ab1db3a7cd96c8e3b7a36ed2efa5b
Tobias Schweizer <t.schweizer@unibas.ch>
2020-07-29 09:37:40 +0200

    feature (ontologies): support string literal in resource class def.

--- src/models/v2/ontologies/resource-class-definition.ts
+++ src/models/v2/ontologies/resource-class-definition.ts
@@ -2,6 +2,7 @@ import { JsonObject, JsonProperty } from "json2typescript";
 import { Constants } from "../Constants";
 import { HasCardinalityForPropertyConverter } from "../custom-converters/has-cardinality-for-property-converter";
 import { SubClassOfConverter } from "../custom-converters/subclass-of-converter";
+import { StringLiteralJsonLd } from "../string-literal-json-ld";
 import { ClassDefinition, IHasProperty } from "./class-definition";
 
 @JsonObject("ResourceClassDefinition")
@@ -23,4 +24,20 @@ export class ResourceClassDefinition extends ClassDefinition {
 
     @JsonProperty(Constants.CanBeInstantiated, Boolean, true)
     canBeInstantiated: boolean = false;
+}
+
+@JsonObject("ResourceClassDefinitionWithAllLanguages")
+export class ResourceClassDefinitionWithAllLanguages extends ResourceClassDefinition {
+
+    @JsonProperty(Constants.Comment, String, true) // TODO: use custom converter: convert string literal or array of string literals to string or undefined
+    comment?: string = undefined;
+
+    @JsonProperty(Constants.Comment, [StringLiteralJsonLd], true) // TODO: use custom converter: convert string literal or array of string literals to an array of string literals
+    comments: [] = [];
+
+    @JsonProperty(Constants.Label, String, true) // TODO: use custom converter: convert string literal or array of string literals to string or undefined
+    label?: string = undefined;
+
+    @JsonProperty(Constants.Label, [StringLiteralJsonLd]) // TODO: use custom converter: convert string literal or array of string literals to an array of string literals
+    labels: [] = [];
 }
