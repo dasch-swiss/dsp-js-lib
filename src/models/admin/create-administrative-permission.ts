bca815c0dd41ea66773e77ae61aec657f1629ca4
Tobias Schweizer <t.schweizer@unibas.ch>
2020-10-06 11:10:01 +0200

    refactor (permissions): separate admin permissions and default object access permissions

--- src/models/admin/create-administrative-permission.ts
+++ src/models/admin/create-administrative-permission.ts
@@ -1,34 +1,17 @@
 import { JsonObject, JsonProperty } from "json2typescript";
+import { CreateAdminDoapBase } from "./create-admin-doap-base";
 import { CreatePermission } from "./create-permission";
 
 /**
  * Creation of an administrative permission.
  */
 @JsonObject("CreateAdministrativePermission")
-export class CreateAdministrativePermission {
-
-    /**
-     * The permissions id, if provided.
-     */
-    @JsonProperty("id", String, true)
-    id?: string;
+export class CreateAdministrativePermission extends CreateAdminDoapBase {
 
     /**
      * The group that the permission applies to.
      */
-    @JsonProperty("forGroup")
-    forGroup: string | null = null;
-
-    /**
-     * The project that the permission applies to.
-     */
-    @JsonProperty("forProject", String)
-    forProject: string = "";
-
-    /**
-     * The permissions granted by an AdministrativePermission.
-     */
-    @JsonProperty("hasPermissions", [CreatePermission])
-    hasPermissions: CreatePermission[] = [];
+    @JsonProperty("forGroup", String)
+    forGroup: string;
 
 }
--- src/models/admin/create-default-object-access-permission.ts
+++ src/models/admin/create-default-object-access-permission.ts
@@ -1,15 +1,28 @@
 import { JsonObject, JsonProperty } from "json2typescript";
+import { CreateAdminDoapBase } from "./create-admin-doap-base";
 import { CreateAdministrativePermission } from "./create-administrative-permission";
 
 /**
  * Creation of a default object access permission permission.
  */
 @JsonObject("CreateDefaultObjectAccessPermission")
-export class CreateDefaultObjectAccessPermission extends CreateAdministrativePermission {
+export class CreateDefaultObjectAccessPermission extends CreateAdminDoapBase {
 
+    /**
+     * The group that the permission applies to.
+     */
+    @JsonProperty("forGroup")
+    forGroup: string | null = null;
+
+    /**
+     * The property that the permission applies to.
+     */
     @JsonProperty("forProperty")
     forProperty: string | null = null;
 
+    /**
+     * The permissions granted by an AdministrativePermission.
+     */
     @JsonProperty("forResourceClass")
     forResourceClass: string | null = null;
 
