cda8d8b066a0261eecb9ff58aed7431ccc0eb0bd
Tobias Schweizer <t.schweizer@unibas.ch>
2020-11-05 09:47:13 +0100

    tests (Makefile): do not prepare test data

--- src/api/v2/values/values-endpoint.spec.ts
+++ src/api/v2/values/values-endpoint.spec.ts
@@ -424,7 +424,7 @@ describe("ValuesEndpoint", () => {
 
         });
 
-        it("should read a date value (Islamic)", done => {
+        fit("should read a date value (Islamic)", done => {
 
             knoraApiConnection.v2.values.getValue("http://rdfh.ch/0001/H6gBWUuJSuuO-CilHV8kQw", "-rG4F5FTTu2iB5mTBPVn5Q").subscribe(
                 (res: ReadResource) => {
