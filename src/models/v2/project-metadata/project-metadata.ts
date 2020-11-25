78669bc6f8bb55da952ce9d9292ff02955ce6d3c
Marcin Procyk <marcin.procyk@dasch.swiss>
2020-11-25 10:15:05 +0100

    fix: package.json formatting

--- src/models/v2/project-metadata/project-metadata.ts
+++ src/models/v2/project-metadata/project-metadata.ts
@@ -58,31 +58,6 @@ export class Dataset {
 
     @JsonProperty(Constants.dspRepoBase + "sameAs", UrlToStringConverter, true)
     sameAs?: string = undefined;
-
-// left to temp test purposes as it was wroking example
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasName", String)
-    // name: string = "";
-
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasFunder", String)
-    // founder: string = "";
-
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasKeywords", [String])
-    // keywords: string[] = [];
-
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasEndDate", String)
-    // endDate: string;
-
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasCategories", String)
-    // categories: string;
-
-    // @JsonProperty("@type", String)
-    // type: string = "";
-
-    // @JsonProperty("http://ns.dasch.swiss/repository#hasStartDate", String)
-    // startDate: string;
-
-    // @JsonProperty("@id", String)
-    // id: string = "";
 }
 
 @JsonObject("ProjectsMetdata")
