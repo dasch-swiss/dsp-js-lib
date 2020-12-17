5b0bac719b6c7666caa6b357ea3307bbee664dd2
Marcin Procyk <marcin.procyk@dasch.swiss>
2020-12-17 10:19:08 +0100

    DSP-1126 Finish metadata endpoint in JS-LIB (#268)
    * refactor: constants names
    
    * refactor: project class name
    
    * fix: preserve id's
    
    * fix: serialization of URL objects + inheritance
    
    * fix: email serialized value
    
    * refactor: split place & definition converters + fix serialization
    
    * fix: person, data management, discipline
    
    * feat: add serialization for person and organization classes
    
    * feat: cover project payload
    
    * fix: funder in project and grant +  person/org converter
    
    * fix: person/org converter
    
    * feat: preserve type
    
    * feat: add complete test data
    
    * refactor: change preserve types implementation
    
    * fix: dataset & project types
    
    * refactor: remove redundand payload + add impoerts + cleanup
    
    * refactor: add docs cateogries
    
    * feat: add mapping references methods
    
    * refactor: add more docs cateogries
    
    * refactor: move metadata util methods to its namespace
    
    * refactor: metadata constants
    
    * fix: base funder class
    
    * refactor: PR minor fixes
    
    * refactor: simplify person/org converter conditions
    
    * refactor: replace email converter by string + remove bas funder abstract
    
    * refactor: constants delimiters
    
    * refactor: add multiple dataset possiility
    
    * refactor: class file names + base class to abstract + minor fixes
    
    * refactor: removed spread operator

---
+++ src/models/v2/project-metadata/single-project.ts
@@ -0,0 +1,75 @@
+import { JsonObject, JsonProperty } from "json2typescript";
+import { Constants } from "../Constants";
+import { AdvancedUrlObjectConverter } from "../custom-converters/advanced-url-object-converter";
+import { DateConverter } from "../custom-converters/date-converter";
+import { PersonOrganizationConverter } from "../custom-converters/person-organization-converter";
+import { UrlToUrlObjectConverter } from "../custom-converters/url-to-url-object-converter";
+import { IUrl } from "../custom-converters/base-url-converter";
+import { BaseProjectMetadata } from "./base-project-metadata";
+import { DataManagementPlan } from "./data-management-plan";
+import { Grant } from "./grant";
+import { Organization } from "./organization";
+import { Person } from "./person";
+import { Place } from "./place";
+
+/** 
+ * @category Model V2 
+ */ 
+@JsonObject("SingleProject")
+export class SingleProject extends BaseProjectMetadata {
+
+    @JsonProperty("@id", String)
+    id: string = "";
+    
+    @JsonProperty(Constants.DspHasAlternateName, String, true)
+    alternateName?: string = undefined;
+
+    @JsonProperty(Constants.DspHasContactPoint, Person, true)
+    contactPoint?: Person = undefined;
+
+    @JsonProperty(Constants.DspHasDataManagementPlan, DataManagementPlan, true)
+    dataManagementPlan?: DataManagementPlan = undefined;
+
+    @JsonProperty(Constants.DspHasDescription, String)
+    description: string = "";
+
+    @JsonProperty(Constants.DspHasDiscipline, AdvancedUrlObjectConverter)
+    discipline: IUrl = {} as IUrl;
+    
+    @JsonProperty(Constants.DspHasEndDate, DateConverter, true)
+    endDate?: string = undefined;
+
+    @JsonProperty(Constants.DspHasFunder, PersonOrganizationConverter)
+    funder: Person | Organization | object = new Person();
+
+    @JsonProperty(Constants.DspHasGrant, Grant, true)
+    grant?: Grant = undefined;
+
+    @JsonProperty(Constants.DspHasKeywords, [String])
+    keywords: string[] = [];
+
+    @JsonProperty(Constants.DspHasName, String)
+    name: string = "";
+
+    @JsonProperty(Constants.DspHasPublication, String, true)
+    publication?: string = undefined;
+
+    @JsonProperty(Constants.DspHasShortcode, String)
+    shortcode: string = "";
+
+    @JsonProperty(Constants.DspHasSpatialCoverage, [Place])
+    spatialCoverage: Place[] = [];
+
+    @JsonProperty(Constants.DspHasStartDate, DateConverter)
+    startDate: string = "";
+
+    @JsonProperty(Constants.DspHasTemporalCoverage, AdvancedUrlObjectConverter)
+    temporalCoverage: IUrl = {} as IUrl;
+
+    @JsonProperty(Constants.DspHasURL, UrlToUrlObjectConverter)
+    url: IUrl = {} as IUrl;
+
+    constructor() {
+        super(Constants.DspProject);
+    }
+}
--- src/models/v2/project-metadata/update-project-metadata.ts
+++ src/models/v2/project-metadata/update-project-metadata.ts
@@ -1,9 +1,12 @@
 import { JsonObject, JsonProperty } from "json2typescript";
 import { Constants } from "../Constants";
 
+/** 
+* @category Model V2 
+*/ 
 @JsonObject("UpdateProjectMetadataResponse")
 export class UpdateProjectMetadataResponse {
 
-    @JsonProperty(Constants.KnoraApiV2 + Constants.Delimiter + "result", String)
+    @JsonProperty(Constants.KnoraApiV2 + Constants.HashDelimiter + "result", String)
     result: string = "";
 }
