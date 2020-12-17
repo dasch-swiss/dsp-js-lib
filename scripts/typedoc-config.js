132273a1f4346eb3a1e2879ea746c097c08d7eec
Tobias Schweizer <t.schweizer@unibas.ch>
2020-12-17 15:21:30 +0100

    docs (github pages): automatic versioned build 2

--- scripts/typedoc-config.js
+++ scripts/typedoc-config.js
@@ -2,9 +2,11 @@
 
 const fs = require('fs-extra');
 
+// append version number to typedoc's out dir config,
+// so that each release has its own API docs directory on gh pages
 const pack = fs.readJsonSync('./package.json');
 const typedoc = fs.readJsonSync('./typedoc.json');
 
 const version = pack.version;
-typedoc.out = "docs/" + version.replace(".", "_");
+typedoc.out = "docs/" + version.replace(".", "_"); // e.g., 1.0.0 -> 1_0_0 (out: docs/1_0_0)
 fs.writeJsonSync('./typedoc.json', typedoc);
