7c09fc1e522a7f4996fd4edeeb0b6fb50df6af14
mdelez <60604010+mdelez@users.noreply.github.com>
2021-01-07 09:20:33 +0100

    DSP-1180 - Support Edit List Feature Toggle Endpoints (#281)
    * feat: lists-endpoint-admin - add support for Put(admin/lists/<listItemIri>) using the feature toggle
    
    * fix (list-endpoint spec): fixed failing test
    
    * moved manually generated test data folder to try to fix tests on github CI
    
    * refactor (update-child-node-request): made the labels and comments properties optional and added an additional unit test
    
    * feat (lists-endpoint-admin): added support for new routes under feature toggle to get node info and create a list
    
    * feat (lists-endpoint-admin): added support for the getting a root node or child node using the feature toggle
    
    * refactor (list-endpoint-admin): replace contents of old methods with calls to new methods to prevent breaking changes
    
    * refactor (lists-endpoint-admin): changed implementations of current methods instead of making new ones
    
    * enhancement (lists-endpoint-admin): throw error if an incomplete UpdateChildNodeRequest is passed to updateChildNode()
    
    * tests (e2e): added e2e tests for new methods
    
    * test (lists-endpoint-admin): added some more assertions
    
    Co-authored-by: Tobias Schweizer <tobiasschweizer@users.noreply.github.com>

--- src/api/admin/lists/lists-endpoint-admin.ts
+++ src/api/admin/lists/lists-endpoint-admin.ts
@@ -10,6 +10,7 @@ import { ListsResponse } from "../../../models/admin/lists-response";
 import { UpdateChildNodeCommentsRequest } from "../../../models/admin/update-child-node-comments-request";
 import { UpdateChildNodeLabelsRequest } from "../../../models/admin/update-child-node-labels-request";
 import { UpdateChildNodeNameRequest } from "../../../models/admin/update-child-node-name-request";
+import { UpdateChildNodeRequest } from "../../../models/admin/update-child-node-request";
 import { UpdateListInfoRequest } from "../../../models/admin/update-list-info-request";
 import { ApiResponseData } from "../../../models/api-response-data";
 import { ApiResponseError } from "../../../models/api-response-error";
@@ -43,34 +44,6 @@ export class ListsEndpointAdmin extends Endpoint {
     
         return this.httpGet("?projectIri=" + encodeURIComponent(projectIri)).pipe(
             map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListsResponse, this.jsonConvert)),
-            catchError(error => this.handleError(error))
-        );
-    
-    }
-    
-    /**
-     * Creates a list.
-     * 
-     * @param listInfo Information about the list to be created.
-     */
-    createList(listInfo: CreateListRequest): Observable<ApiResponseData<ListResponse> | ApiResponseError> {
-    
-        return this.httpPost("", this.jsonConvert.serializeObject(listInfo)).pipe(
-            map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListResponse, this.jsonConvert)),
-            catchError(error => this.handleError(error))
-        );
-    
-    }
-    
-    /**
-     * Gets a list.
-     * 
-     * @param iri The IRI of the list.
-     */
-    getList(iri: string): Observable<ApiResponseData<ListResponse> | ApiResponseError> {
-    
-        return this.httpGet("/" + encodeURIComponent(iri)).pipe(
-            map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListResponse, this.jsonConvert)),
             catchError(error => this.handleError(error))
         );
     
@@ -162,16 +135,64 @@ export class ListsEndpointAdmin extends Endpoint {
         );
     
     }
-    
+
+    // *** All methods below this point require the "new-list-admin-routes:1=on" feature toggle ***
+
     /**
-     * Returns information about a list node.
+     * Updates a child node.
      * 
-     * @param iri The IRI of the node.
+     * @param nodeInfo Information about the node to be updated.
      */
-    getListNodeInfo(iri: string): Observable<ApiResponseData<ListNodeInfoResponse> | ApiResponseError> {
+    updateChildNode(nodeInfo: UpdateChildNodeRequest): Observable<ApiResponseData<ChildNodeInfoResponse> | ApiResponseError> {
     
-        return this.httpGet("/nodes/" + encodeURIComponent(iri)).pipe(
+        if (nodeInfo.name === undefined && nodeInfo.labels === undefined && nodeInfo.comments === undefined) {
+            throw new Error("At least one property is expected from the following properties: name, labels, comments.");
+        }
+
+        return this.httpPut("/" + encodeURIComponent(nodeInfo.listIri), this.jsonConvert.serializeObject(nodeInfo), undefined, { "X-Knora-Feature-Toggles": "new-list-admin-routes:1=on" }).pipe(
+            map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ChildNodeInfoResponse, this.jsonConvert)),
+            catchError(error => this.handleError(error))
+        );
+    
+    }
+
+    /**
+     * Returns information about a list node using the new route.
+     * 
+     * @param listItemIri The IRI of the node.
+     */
+    getListNodeInfo(listItemIri: string): Observable<ApiResponseData<ListNodeInfoResponse> | ApiResponseError> {
+    
+        return this.httpGet("/" + encodeURIComponent(listItemIri) + "/info", { "X-Knora-Feature-Toggles": "new-list-admin-routes:1=on" }).pipe(
             map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListNodeInfoResponse, this.jsonConvert)),
+            catchError(error => this.handleError(error))
+        );
+    
+    }
+
+    /**
+     * Creates a list using the old route but with the feature toggle enabled.
+     * 
+     * @param listInfo Information about the list to be created.
+     */
+    createList(listInfo: CreateListRequest): Observable<ApiResponseData<ListResponse> | ApiResponseError> {
+    
+        return this.httpPost("", this.jsonConvert.serializeObject(listInfo), undefined, { "X-Knora-Feature-Toggles": "new-list-admin-routes:1=on" }).pipe(
+            map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListResponse, this.jsonConvert)),
+            catchError(error => this.handleError(error))
+        );
+    
+    }
+
+    /**
+     * Gets a list using the old route but with the feature toggle enabled
+     * 
+     * @param listItemIri The IRI of the list.
+     */
+    getList(listItemIri: string): Observable<ApiResponseData<ListResponse> | ApiResponseError> {
+    
+        return this.httpGet("/" + encodeURIComponent(listItemIri), { "X-Knora-Feature-Toggles": "new-list-admin-routes:1=on" }).pipe(
+            map(ajaxResponse => ApiResponseData.fromAjaxResponse(ajaxResponse, ListResponse, this.jsonConvert)),
             catchError(error => this.handleError(error))
         );
     
