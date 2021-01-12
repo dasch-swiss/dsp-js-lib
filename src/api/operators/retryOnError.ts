c2fa0b3db9f0dcea8e721c240eb717ae62ae251e
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2021-01-12 17:26:29 +0100

    feat(endpoint): Retry failed HTTP requests (DSP-170) (#286)
    * feature (endpoint): retry failed HTTP requests
    
    * feature (endpoint): retry failed HTTP requests
    
    * tests (endpoint): test retryOnError
    
    * tests (endpoint): test retryOnError
    
    * refactor (endpoint): only delay actual retries
    
    * tests (endpoint): test retryOnError
    
    * tests (endpoint): test retryOnError
    
    * refactor (endpoint): retry all failed HTTP request verbs
    
    * tests (endpoint): test retryOnError
    
    * refactor (endpoint): use explicit type annotation
    
    * docs (endpoint): add some documentation

---
+++ src/api/operators/retryOnError.ts
@@ -0,0 +1,43 @@
+import { Observable, of, throwError } from "rxjs";
+import { AjaxError, AjaxResponse } from "rxjs/ajax";
+import { delay, mergeMap, retryWhen, tap } from "rxjs/operators";
+
+/**
+ *
+ * Retries failed HTTP requests.
+ *
+ * @param delayMs delay in milliseconds before the request is retried.
+ * @param maxRetries maximum number of retries.
+ * @param retryOnErrorStatus HTTP error status codes for which the request is retried.
+ * @param logError if true, error is written to the console error log.
+ *
+ * @category Internal
+ */
+export function retryOnError(delayMs: number, maxRetries: number, retryOnErrorStatus: number[], logError: boolean) {
+    let retries = maxRetries;
+
+    // inspired by https://medium.com/angular-in-depth/retry-failed-http-requests-in-angular-f5959d486294
+    return (src: Observable<AjaxResponse>): Observable<AjaxResponse> =>
+        src.pipe(
+            // when no error is thrown, this simply returns the source Observable
+            retryWhen(errors =>
+                errors.pipe(
+                    // log error message if logging is enabled
+                    tap((error: AjaxError) => {
+                        if (logError) console.error("HTTP request failed:", "status:", error.status, "retries:", retries, "error:", error);
+                    }),
+                    mergeMap((error: AjaxError) => {
+                        // retry on specified error status
+                        // check if max retries is reached ("retries" is decremented on each retry)
+                        if (retryOnErrorStatus.indexOf(error.status) !== -1 && retries-- > 0) {
+                            return of(error).pipe(
+                                // delay retry
+                                delay(delayMs)
+                            );
+                        } else {
+                            // do not retry
+                            return throwError(error);
+                        }
+                    })
+                )));
+}
