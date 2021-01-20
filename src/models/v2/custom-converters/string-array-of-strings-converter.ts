81f7cce27aaa99d7dad3a96f8b373ac297b7fbf1
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-01-20 12:31:39 +0100

    feat: update x-n string properties in SingleProject + strings converter

---
+++ src/models/v2/custom-converters/string-array-of-strings-converter.ts
@@ -0,0 +1,28 @@
+import { JsonConverter, JsonCustomConvert } from "json2typescript";
+
+type ElementOrArrayOfElements<T> = T | T[];
+
+/**
+ * @category Internal
+ */
+@JsonConverter
+export class StringArrayOfStringsConverter implements JsonCustomConvert<ElementOrArrayOfElements<string>> {
+
+    serialize<T>(el: ElementOrArrayOfElements<T>): any {
+        console.log("SER", el);
+        if (Array.isArray(el)) {
+            return el as T[];
+        } else {
+            return el;
+        }
+    }
+
+    deserialize<T>(el: any): ElementOrArrayOfElements<T> {
+        console.log("deSER", el);
+        if (Array.isArray(el)) {
+            return el as T[];
+        } else {
+            return el;
+        }
+    }
+}
--- src/models/v2/project-metadata/single-project.ts
+++ src/models/v2/project-metadata/single-project.ts
@@ -1,10 +1,11 @@
 import { JsonObject, JsonProperty } from "json2typescript";
 import { Constants } from "../Constants";
 import { AdvancedUrlObjectConverter } from "../custom-converters/advanced-url-object-converter";
+import { IUrl } from "../custom-converters/base-url-converter";
 import { DateConverter } from "../custom-converters/date-converter";
 import { PersonOrganizationConverter } from "../custom-converters/person-organization-converter";
+import { StringArrayOfStringsConverter } from "../custom-converters/string-array-of-strings-converter";
 import { UrlToUrlObjectConverter } from "../custom-converters/url-to-url-object-converter";
-import { IUrl } from "../custom-converters/base-url-converter";
 import { BaseProjectMetadata } from "./base-project-metadata";
 import { DataManagementPlan } from "./data-management-plan";
 import { Grant } from "./grant";
@@ -21,8 +22,8 @@ export class SingleProject extends BaseProjectMetadata {
     @JsonProperty("@id", String)
     id: string = "";
     
-    @JsonProperty(Constants.DspHasAlternateName, String, true)
-    alternateName?: string = undefined;
+    @JsonProperty(Constants.DspHasAlternateName, StringArrayOfStringsConverter, true)
+    alternateName?: string[] | string = undefined;
 
     @JsonProperty(Constants.DspHasContactPoint, Person, true)
     contactPoint?: Person = undefined;
@@ -33,6 +34,7 @@ export class SingleProject extends BaseProjectMetadata {
     @JsonProperty(Constants.DspHasDescription, String)
     description: string = "";
 
+    // 1-n
     @JsonProperty(Constants.DspHasDiscipline, AdvancedUrlObjectConverter)
     discipline: IUrl = {} as IUrl;
     
@@ -42,30 +44,34 @@ export class SingleProject extends BaseProjectMetadata {
     @JsonProperty(Constants.DspHasFunder, PersonOrganizationConverter)
     funder: Person | Organization | object = new Person();
 
+    // 0-n
     @JsonProperty(Constants.DspHasGrant, Grant, true)
     grant?: Grant = undefined;
 
-    @JsonProperty(Constants.DspHasKeywords, [String])
-    keywords: string[] = [];
+    @JsonProperty(Constants.DspHasKeywords, StringArrayOfStringsConverter)
+    keywords: string[] | string = [];
 
     @JsonProperty(Constants.DspHasName, String)
     name: string = "";
 
-    @JsonProperty(Constants.DspHasPublication, String, true)
-    publication?: string = undefined;
+    @JsonProperty(Constants.DspHasPublication, StringArrayOfStringsConverter, true)
+    publication?: string[] | string = undefined;
 
     @JsonProperty(Constants.DspHasShortcode, String)
     shortcode: string = "";
 
+    // 1-n
     @JsonProperty(Constants.DspHasSpatialCoverage, [Place])
     spatialCoverage: Place[] = [];
 
     @JsonProperty(Constants.DspHasStartDate, DateConverter)
     startDate: string = "";
 
+    // 1-n
     @JsonProperty(Constants.DspHasTemporalCoverage, AdvancedUrlObjectConverter)
     temporalCoverage: IUrl = {} as IUrl;
 
+    // 1-2
     @JsonProperty(Constants.DspHasURL, UrlToUrlObjectConverter)
     url: IUrl = {} as IUrl;
 
