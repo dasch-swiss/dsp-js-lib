fe8941bd63d670de6516424906cd8db157a7194e
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-01-25 17:49:49 +0100

    refactor(Attribution): replace Place by PersonOrganizationConverter

--- src/models/v2/project-metadata/attribution.ts
+++ src/models/v2/project-metadata/attribution.ts
@@ -1,8 +1,10 @@
 import { JsonObject, JsonProperty } from "json2typescript";
 import { Constants } from "../Constants";
-import { IdConverter } from "../custom-converters/id-converter";
+import { PersonOrganizationConverter } from "../custom-converters/person-organization-converter";
 import { StringArrayOfStringsConverter } from "../custom-converters/string-array-of-strings-converter";
 import { BaseProjectMetadata } from "./base-project-metadata";
+import { Organization } from "./organization";
+import { Person } from "./person";
 
 /** 
  * @category Model V2 
@@ -13,8 +15,8 @@ export class Attribution extends BaseProjectMetadata {
     @JsonProperty(Constants.DspHasRole, StringArrayOfStringsConverter)
     role: string[] | string = [];
 
-    @JsonProperty(Constants.ProvAgent, IdConverter)
-    agent: string = "";
+    @JsonProperty(Constants.ProvAgent, PersonOrganizationConverter)
+    agent: Person | Organization | object = new Person();
 
     constructor() {
         super(Constants.ProvAttribution);
