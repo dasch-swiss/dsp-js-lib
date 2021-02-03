4096298fd3639d735fd51caac9c396f8f7e4aaa5
Marcin Procyk <marcin.procyk@dasch.swiss>
2021-02-03 11:38:05 +0100

    feat(UnionElementArrayOfElementsConverter): replace switch with if else

--- src/models/v2/custom-converters/union-element-array-of-elements-converter.ts
+++ src/models/v2/custom-converters/union-element-array-of-elements-converter.ts
@@ -22,70 +22,65 @@ export class UnionElementArrayOfElementsConverter implements JsonCustomConvert
     
     serialize(el: Attribution[] | Place[] | Grant[]| object): any {
         if (Array.isArray(el)) {
-            switch (true) {
-                case el[0].hasOwnProperty("type") && (el[0] as {type: string})["type"] === Constants.ProvAttribution:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Attribution);
-                case el[0].hasOwnProperty("type") && (el[0] as Grant)["type"] === Constants.DspGrant:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Grant);
-                case el[0].hasOwnProperty("place"):
-                    return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Place);
-                default:
-                    throw new Error(`Serialization Error: expected Attribution[], Grant[] or Place[] type. 
-                        Instead got ${typeof el}.`);
+            if (el[0].hasOwnProperty("type") && (el[0] as {type: string})["type"] === Constants.ProvAttribution) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Attribution);
+            } else if (el[0].hasOwnProperty("type") && (el[0] as Grant)["type"] === Constants.DspGrant) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Grant);
+            } else if (el[0].hasOwnProperty("place")) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.serializeArray(el, Place);
+            } else {
+                throw new Error(`Serialization Error: expected Attribution[], Grant[] or Place[] type. 
+                Instead got ${typeof el}.`);
             }
         } else {
-            switch (true) {
-                case el.hasOwnProperty("id"):
-                    return {
-                        "@id": (el as { [index: string]: string })["id"]
-                    };
-                default:
-                    throw new Error(`Serialization Error: expected reference object with id key. 
-                        Instead got ${typeof el}.`);
+            if (el.hasOwnProperty("id")) {
+                return {
+                    "@id": (el as { [index: string]: string })["id"]
+                };
+            } else {
+                throw new Error(`Serialization Error: expected reference object with id key. Instead got ${typeof el}.`);
             }
         }
     }
 
     deserialize(el: any ): Attribution | Grant | Place | Attribution[] | Grant[] | Place[] | IId | IId[] {
         if (Array.isArray(el)) {
-            switch (true) {
-                case el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.ProvAttribution:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Attribution);
-                case el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.DspGrant:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Grant);
-                case (el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.SchemaPlace) 
-                // below condition is temp solution for data sent from the test app, due to different structure
-                    || el[0].hasOwnProperty(Constants.SchemaUrlValue):
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Place);
-                case el[0].hasOwnProperty("@id"):
-                    const newArr = [] as IId[];
-                    if (Array.isArray(el)) {
-                        el.forEach((
-                            (item: any) => newArr.push({id: item})
-                        ));
-                    }
-                    return newArr;
-                default:
-                    throw new Error(`Deserialization Error: expected an array of objects with property @type equals to: 
-                        ${Constants.ProvAttribution}, ${Constants.DspGrant}, ${Constants.SchemaPlace}, or a reference 
-                        object with @id key.`);
+            if (el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.ProvAttribution) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Attribution);
+            } else if (el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.DspGrant) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Grant);
+            } else if ((el[0].hasOwnProperty("@type") && el[0]["@type"] === Constants.SchemaPlace) 
+            // below condition is temp solution for data sent from the test app, due to different structure
+                || el[0].hasOwnProperty(Constants.SchemaUrlValue)) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray(el, Place);
+            } else if (el[0].hasOwnProperty("@id")) {
+                const newArr = [] as IId[];
+                if (Array.isArray(el)) {
+                    el.forEach(
+                        (item: any) => newArr.push({id: item})
+                    );
+                }
+                return newArr;
+            } else {
+                throw new Error(`Deserialization Error: expected an array of objects with property @type equals to: 
+                    ${Constants.ProvAttribution}, ${Constants.DspGrant}, ${Constants.SchemaPlace}, or a reference 
+                    object with @id key.`);
             }
         } else {
-            switch (true) {
-                case el.hasOwnProperty("@type") && el["@type"] === Constants.ProvAttribution:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Attribution);
-                case el.hasOwnProperty("@type") && el["@type"] === Constants.DspGrant:
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Grant);
-                case (el.hasOwnProperty("@type") && el["@type"] === Constants.SchemaPlace) 
-                // below condition is temp solution for data sent from the test app, due to different structure
-                    || el.hasOwnProperty(Constants.SchemaUrlValue):
-                    return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Place);
-                case el.hasOwnProperty("@id"):
-                    return {id: el};
-                default:
-                    throw new Error(`Deserialization Error: expected an object with property @type equals to: 
-                        ${Constants.ProvAttribution}, ${Constants.DspGrant}, ${Constants.SchemaPlace}, or a reference 
-                        object with id key`);
+            if (el.hasOwnProperty("@type") && el["@type"] === Constants.ProvAttribution) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Attribution);
+            } else if (el.hasOwnProperty("@type") && el["@type"] === Constants.DspGrant) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Grant);
+            } else if ((el.hasOwnProperty("@type") && el["@type"] === Constants.SchemaPlace) 
+            // below condition is temp solution for data sent from the test app, due to different structure
+                || el.hasOwnProperty(Constants.SchemaUrlValue)) {
+                return UnionElementArrayOfElementsConverter.jsonConvert.deserializeArray([el], Place);
+            } else if (el.hasOwnProperty("@id")) {
+                return {id: el};
+            } else {
+                throw new Error(`Deserialization Error: expected an object with property @type equals to: 
+                ${Constants.ProvAttribution}, ${Constants.DspGrant}, ${Constants.SchemaPlace}, or a reference 
+                object with id key`);
             }
         }
     }
