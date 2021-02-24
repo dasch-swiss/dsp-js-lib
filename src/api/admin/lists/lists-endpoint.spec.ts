4f14605b6dac16de30a3b0b70cd6d134f89bc2ed
mdelez <60604010+mdelez@users.noreply.github.com>
2021-02-24 09:36:13 +0100

    test(lists-endpoint): adds one more assertion

--- src/api/admin/lists/lists-endpoint.spec.ts
+++ src/api/admin/lists/lists-endpoint.spec.ts
@@ -433,6 +433,7 @@ describe("ListsEndpoint", () => {
             knoraApiConnection.admin.listsEndpoint.createChildNode(childNode).subscribe(
                 (res: ApiResponseData<ListNodeInfoResponse>) => {
                     expect(res.body.nodeinfo.id).toEqual('http://rdfh.ch/lists/0001/fdjCFwqKRXexvrSPYLN0cg');
+                    expect(res.body.nodeinfo.position).toEqual(1);
                     done();
                 }
             );
