a91bc49a9c1dda4ce62156c822eebf8845bb52b4
mdelez <60604010+mdelez@users.noreply.github.com>
2021-11-25 11:34:19 +0100

    test(resource-endpoint): update timestamps in test

--- src/api/v2/resource/resources-endpoint.spec.ts
+++ src/api/v2/resource/resources-endpoint.spec.ts
@@ -450,7 +450,7 @@ describe("ResourcesEndpoint", () => {
 
             updateResourceMetadata.hasPermissions = "CR knora-admin:Creator|M knora-admin:ProjectMember|V knora-admin:ProjectMember";
 
-            updateResourceMetadata.newModificationDate = "2021-09-30T16:41:46.980756Z";
+            updateResourceMetadata.newModificationDate = "2021-11-26T08:39:03.827765Z";
 
             knoraApiConnection.v2.res.updateResourceMetadata(updateResourceMetadata).subscribe(
                 (res: UpdateResourceMetadataResponse) => {
@@ -458,7 +458,7 @@ describe("ResourcesEndpoint", () => {
                     expect(res.resourceIri).toEqual("http://rdfh.ch/0001/a-thing");
                     expect(res.resourceClassIri).toEqual("http://0.0.0.0:3333/ontology/0001/anything/v2#Thing");
                     expect(res.hasPermissions).toEqual("CR knora-admin:Creator|M knora-admin:ProjectMember|V knora-admin:ProjectMember");
-                    expect(res.lastModificationDate).toEqual("2021-09-30T16:41:46.980756Z");
+                    expect(res.lastModificationDate).toEqual("2021-11-26T08:39:03.827765Z");
                     done();
                 }
             );
@@ -474,7 +474,7 @@ describe("ResourcesEndpoint", () => {
             const expectedPayload = require("../../../../test/data/api/v2/resources/update-resource-metadata-request-expanded.json");
 
             // has to be done in order not to have to update this test after every release of dsp-api
-            expectedPayload["http://api.knora.org/ontology/knora-api/v2#newModificationDate"]["@value"] = "2021-09-30T16:41:46.980756Z";
+            expectedPayload["http://api.knora.org/ontology/knora-api/v2#newModificationDate"]["@value"] = "2021-11-26T08:39:03.827765Z";
 
             expect(request.data()).toEqual(expectedPayload);
 
