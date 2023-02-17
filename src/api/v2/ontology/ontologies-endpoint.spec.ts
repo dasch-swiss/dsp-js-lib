7beb09a832834a5d35ac6793097df773d93d176d
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-02-17 12:01:47 +0100

    chore: bump dsp-api to 27.0.0 (#501)
    * bunp api
    
    * get test data
    
    * fix tests
    
    * fix tests

--- src/api/v2/ontology/ontologies-endpoint.spec.ts
+++ src/api/v2/ontology/ontologies-endpoint.spec.ts
@@ -1220,7 +1220,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-02-03T09:14:21.914610454Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-02-17T09:07:42.756600653Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
@@ -1265,7 +1265,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-02-03T09:14:21.914610454Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-02-17T09:07:42.756600653Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
