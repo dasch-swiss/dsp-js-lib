f6ec37e4b533e9fccb07e2f5009e899ea3f06dd7
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-10-13 11:06:23 +0200

    update dsp-api to 30.4.0 (#560)

--- src/api/v2/ontology/ontologies-endpoint.spec.ts
+++ src/api/v2/ontology/ontologies-endpoint.spec.ts
@@ -1220,7 +1220,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-09-29T07:39:34.306092476Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-13T08:20:36.025292416Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
@@ -1265,7 +1265,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-09-29T07:39:34.306092476Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-13T08:20:36.025292416Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
--- src/api/v2/resource/resources-endpoint.spec.ts
+++ src/api/v2/resource/resources-endpoint.spec.ts
@@ -486,7 +486,7 @@ describe("ResourcesEndpoint", () => {
                     expect(res.hasPermissions).toEqual("CR knora-admin:Creator|M knora-admin:ProjectMember|V knora-admin:ProjectMember");
 
                     // update this timestamp after every release of dsp-api
-                    expect(res.lastModificationDate).toEqual("2023-09-30T07:36:22.147323365Z");
+                    expect(res.lastModificationDate).toEqual("2023-10-14T08:17:19.432943413Z");
                     done();
                 }
             );
@@ -550,7 +550,7 @@ describe("ResourcesEndpoint", () => {
             updateResourceMetadata.lastModificationDate = "2021-09-30T16:41:46.980756Z";
 
             // update this timestamp after every release of dsp-api
-            updateResourceMetadata.newModificationDate = "2023-09-30T07:36:22.701581932Z";
+            updateResourceMetadata.newModificationDate = "2023-10-14T08:17:19.977353963Z";
 
             knoraApiConnection.v2.res.updateResourceMetadata(updateResourceMetadata).subscribe(
                 (res: UpdateResourceMetadataResponse) => {
@@ -560,7 +560,7 @@ describe("ResourcesEndpoint", () => {
                     expect(res.hasPermissions).toEqual("CR knora-admin:ProjectMember|V knora-admin:ProjectMember");
 
                     // update this timestamp after every release of dsp-api
-                    expect(res.lastModificationDate).toEqual("2023-09-30T07:36:22.701581932Z");
+                    expect(res.lastModificationDate).toEqual("2023-10-14T08:17:19.977353963Z");
                     done();
                 }
             );
@@ -578,7 +578,7 @@ describe("ResourcesEndpoint", () => {
             expectedPayload["http://api.knora.org/ontology/knora-api/v2#lastModificationDate"]["@value"] = "2021-09-30T16:41:46.980756Z";
 
             // update this timestamp after every release of dsp-api
-            expectedPayload["http://api.knora.org/ontology/knora-api/v2#newModificationDate"]["@value"] = "2023-09-30T07:36:22.701581932Z";
+            expectedPayload["http://api.knora.org/ontology/knora-api/v2#newModificationDate"]["@value"] = "2023-10-14T08:17:19.977353963Z";
 
             expect(request.data()).toEqual(expectedPayload);
 
