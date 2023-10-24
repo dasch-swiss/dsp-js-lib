524d1be20087cea9e4ee53b791972608894f16bd
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-10-24 14:38:11 +0200

    chore!: remove zio endpoint implementation

--- src/knora-api-config.ts
+++ src/knora-api-config.ts
@@ -23,32 +23,12 @@ export class KnoraApiConfig {
     }
 
     /**
-     * The full zio http API URL
-     */
-    get zioApiUrl(): string {
-        return (
-            (this.apiProtocol + "://" + this.apiHost) +
-            this.zioPrefix +
-            this.apiPath
-        );
-    }
-
-    /**
-     * List of endpoints using zio http
-     */
-    get zioEndpointsList(): string[] {
-        return this.zioEndpoints;
-    }
-
-    /**
      * @param apiProtocol the protocol of the API (http or https)
      * @param apiHost the DSP-API base URL
      * @param apiPort the port of DSP-API
      * @param apiPath the base path following host and port, if any.
      * @param jsonWebToken token to identify the user
      * @param logErrors determines whether errors should be logged to the console
-     * @param zioPrefix prefix for zio routes, ":5555" is used locally whereas "/zio" is used on servers
-     * @param zioEndpoints list of zio endpoints
      */
     constructor(public apiProtocol: "http" | "https",
                 public apiHost: string,
@@ -56,8 +36,6 @@ export class KnoraApiConfig {
                 public apiPath: string = "",
                 public jsonWebToken: string = "",
                 public logErrors: boolean = false,
-                public zioPrefix: "/zio" | ":5555" = "/zio",
-                public zioEndpoints: string[] = []
     ) {
 
         // Remove port in case it's the default one
