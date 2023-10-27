4535e92343c74a331e9dfd2aef49f6a85becc0cd
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-10-27 09:32:38 +0200

    chore(deps): bump DSP-API version to 30.5.0

--- src/api/v2/ontology/ontologies-endpoint.spec.ts
+++ src/api/v2/ontology/ontologies-endpoint.spec.ts
@@ -1220,7 +1220,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-13T08:20:36.025292416Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-27T07:14:58.982383268Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
@@ -1265,7 +1265,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-13T08:20:36.025292416Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-27T07:14:58.982383268Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
