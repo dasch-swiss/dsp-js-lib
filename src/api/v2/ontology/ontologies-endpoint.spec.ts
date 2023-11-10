0eb07461e016aa7772c85b1a5720f9825a22463e
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-11-10 10:06:23 +0100

    chore: update dsp-api to v30.5.1 (#577)
    * update dsp-api version
    
    * update tests

--- src/api/v2/ontology/ontologies-endpoint.spec.ts
+++ src/api/v2/ontology/ontologies-endpoint.spec.ts
@@ -1220,7 +1220,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-27T07:14:58.982383268Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-11-10T07:57:35.395683684Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
@@ -1265,7 +1265,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-10-27T07:14:58.982383268Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2023-11-10T07:57:35.395683684Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
