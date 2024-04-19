f7bc1db57b9a19423566ef0bab8070ad99d2c9b4
Marcin Procyk <marcin.procyk@dasch.swiss>
2024-04-19 11:31:20 +0200

    update api and fix datestamps

--- src/api/v2/ontology/ontologies-endpoint.spec.ts
+++ src/api/v2/ontology/ontologies-endpoint.spec.ts
@@ -1122,7 +1122,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2024-04-05T05:48:43.136018292Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2024-04-19T09:02:34.427565872Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
@@ -1167,7 +1167,7 @@ describe("OntologiesEndpoint", () => {
             deleteCardinalitiesFromClassRequest.id = "http://0.0.0.0:3333/ontology/0001/freetest/v2";
 
             // update this timestamp after every release of dsp-api
-            deleteCardinalitiesFromClassRequest.lastModificationDate = "2024-04-05T05:48:43.136018292Z";
+            deleteCardinalitiesFromClassRequest.lastModificationDate = "2024-04-19T09:02:34.427565872Z";
 
             const cardinalityToRemove = new UpdateResourceClassCardinality();
 
