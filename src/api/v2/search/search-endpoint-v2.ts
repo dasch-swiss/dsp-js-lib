e38c7f8e4040ac2333158efbf0ae5c43149c5ec3
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-03-28 11:44:01 +0100

    review feedback

--- src/api/v2/search/search-endpoint-v2.ts
+++ src/api/v2/search/search-endpoint-v2.ts
@@ -177,8 +177,14 @@ export class SearchEndpointV2 extends Endpoint {
         );
     }
 
+    /**
+     * Performs a Gravsearch in order to get incoming links of queried resource
+     * 
+     * @param resourceIri resource that is queried for incoming links
+     * @param offset the offset to be used for paging
+     */
     doSearchIncomingLinks(resourceIri: string, offset = 0) {
-        return this.httpGet("/searchIncomingLinks/" + encodeURIComponent(resourceIri) + `?offset=${offset}`).pipe(
+        return this.httpGet(`/searchIncomingLinks/ + ${encodeURIComponent(resourceIri)} + ?offset=${offset}`).pipe(
             mergeMap((response: AjaxResponse) => {
                 return jsonld.compact(response.response, {});
             }), mergeMap((jsonld: object) => {
