7240f832ddf7bcbf9d1a2432bf51653dc2de8388
domsteinbach <dominique.steinbach@unibas.ch>
2025-09-24 07:50:44 +0200

    refactor: remove system endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ export { KnoraApiConfig } from "./knora-api-config";
 
 // System Endpoints
 export { HealthEndpointSystem } from "./api/system/health/health-endpoint-system";
-export { VersionEndpointSystem } from "./api/system/version/version-endpoint-system";
 
 // Admin Endpoints
 export { GroupsEndpointAdmin } from "./api/admin/groups/groups-endpoint-admin";
